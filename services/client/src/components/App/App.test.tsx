d10eacc0bc9b79c34591aab7e5016e8ec643713e
chimailo <collins.chima.ilo@gmail.com>
2021-05-13 17:40:10 +0100

    modified travis.yml

--- services/client/src/components/App/App.test.tsx
+++ services/client/src/components/App/App.test.tsx
@@ -4,6 +4,6 @@ import App from '.';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const el = screen.getByText(/see something funny/i);
+  expect(el).toBeInTheDocument();
 });
